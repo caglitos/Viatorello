10e60ccef843339fc4189102758a7a059f2b631d
Carlos <c4rlosruiz01@gmail.com>
2025-03-16 20:16:59 -0600

    alpha 1.0.1.1

--- app/src/main/java/com/example/vitorello/busActivity.kt
+++ app/src/main/java/com/example/vitorello/busActivity.kt
@@ -3,8 +3,6 @@ package com.example.vitorello
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class busActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
